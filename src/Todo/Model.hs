81debe88ccf30b00500f8813a7affdc77233f4ad
Stanislava Gera <stasya.gera@yandex.ru>
2018-04-01 12:02:08 +0300

    [#4] draft, needs further work

--- src/Todo/Model.hs
+++ src/Todo/Model.hs
@@ -7,18 +7,30 @@ module Todo.Model
     , Deadline (..)
     , Tags
     , Todos
+    , TodoDB (..)
+    , addTodo
+    , addTag
+    , deleteTag
+    , editTag
+    , defTodo
+    , getByState
+    , getTopPrio
+    , getByTag
+    , tagStatistics
     ) where
 
-import           Data.List.NonEmpty (NonEmpty (..))
+import           Data.List.NonEmpty (NonEmpty (..), toList, fromList)
 import qualified Data.Ratio         as R
 import qualified Numeric.Natural    as N
+import qualified Data.List          as List
+-- import qualified Data.List.Unique   as List.Unique
 
 data State = TODO | DONE | FAIL deriving (Eq, Show)
 
 newtype Title = Title (NonEmpty Char) deriving (Eq, Show)
 -- the lower the priority value, the more urgent the task
 newtype Priority = Priority (R.Ratio N.Natural) deriving (Eq, Show, Ord)
-newtype Tag = Tag (NonEmpty Char) deriving (Eq, Show)
+newtype Tag = Tag (NonEmpty Char) deriving (Eq, Show, Ord)
 newtype Deadline = Deadline Int deriving (Eq, Show, Ord)
 type Tags = NonEmpty Tag
 type Todos = [Todo]
@@ -32,3 +44,54 @@ data Todo = Todo
     , todoDeadline    :: Maybe Deadline
     , todoDescription :: Maybe String
     } deriving (Eq, Show)
+
+data TodoDB = TodoDB 
+    { tododbTodos :: Todos
+    , tododbTags  :: [Tag]
+    } deriving (Show)
+
+
+addTodo :: Todo -> TodoDB -> TodoDB
+addTodo todo TodoDB { .. } = TodoDB { tododbTodos = todo : tododbTodos, .. }
+
+addTag :: Tag -> TodoDB -> TodoDB
+addTag tag TodoDB { .. } = TodoDB { tododbTags = tag : tododbTags, .. }
+
+deleteTag :: Tag -> TodoDB -> TodoDB
+deleteTag tag db@TodoDB { .. } = 
+    case List.find (\todo -> todoTags todo == tag :| []) (getByTag tag tododbTodos) 
+    of Nothing -> TodoDB { tododbTodos = map (\Todo{ .. } -> Todo { todoTags = fromList $ (List.delete tag) . toList $ todoTags, .. }) tododbTodos
+                         , tododbTags  = List.delete tag tododbTags }
+       Just _ -> db
+
+editTag :: Tag -> Tag -> TodoDB -> TodoDB
+editTag old new TodoDB{ .. } = 
+    TodoDB { tododbTodos = map (\Todo{ .. } -> Todo { todoTags = fromList $ replace $ toList todoTags, .. }) tododbTodos
+           , tododbTags  = new : List.delete old tododbTags } where 
+            replace :: [Tag] -> [Tag]
+            replace [] = []
+            replace (t:ts) = if t == old then new : ts else t : replace ts
+
+
+defTodo :: Title -> Priority -> Tags -> Todo
+defTodo todoTitle todoPriority todoTags = Todo { todoState = TODO
+                                               , todoSubtasks = []
+                                               , todoDeadline = Nothing
+                                               , todoDescription = Nothing
+                                               , .. }
+
+getByState :: State -> Todos -> Todos
+getByState state = filter $ (state ==) . todoState
+
+getByTag :: Tag -> Todos -> Todos
+getByTag tag = filter $ elem tag . todoTags
+
+getTopPrio :: Int -> Todos -> Todos
+getTopPrio n lst = take n (List.sortBy ((. todoPriority) . compare . todoPriority) lst)
+
+tagStatistics :: Todos -> [(Tag, Int)]
+tagStatistics = count . concatMap (toList . todoTags)
+    where 
+        -- from Data.List.Unique
+        count :: Ord a => [a] -> [(a, Int)]
+        count = map (\x -> (head x, length x)) . List.group . List.sort
